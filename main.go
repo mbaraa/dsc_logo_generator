e4b4e41731e998af92cb2a2e276f40d220324e8c
Baraa Al-Masri <hexagon16rpm@yandex.com>
2021-07-30 12:27:20 +0300

    meh

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ func main() {
 	startServer()
 }
 
-// startServer sets end-points and sets CORS for JS :)
-// then start the server
 func startServer() {
 	router := http.NewServeMux()
 	router.Handle("/", http.FileServer(http.Dir("./client/dist/")))
-
 	router.HandleFunc("/api/genlogo/", api.GetLogo) // url?uni_name=someName&img_color=colored|gray|white&opacity=1|0&logo_type=1|2
+	
 	log.Fatal(http.ListenAndServe(":1105", router))
 }
