1d5bcf379e429c9536b595a6790c1440d1e3d252
Baraa Al-Masri <m.baraa.masri@gmail.com>
2022-04-18 21:17:41 +0300

    Update main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store/store";
 
-export const backendAddress = "https://logogen.mbaraa.fun"
+export const backendAddress = "https://logogen.sheev.xyz"
 
 createApp(App)
     .use(store)
